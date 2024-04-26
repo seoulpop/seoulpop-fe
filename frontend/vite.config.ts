27e015410e841d68b788c8f98fc08c4016eac1a6
Asher Seo <beomxtone@gmail.com>
2024-04-26 12:16:32 +0900

    chore: change build dir build to dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   plugins: [react(), svgr()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   resolve: {
     alias: [
