493d69bbccb1a580ea182ff40679fea5930cdcab
parkyolo <20181621@kookmin.ac.kr>
2024-04-28 23:23:03 +0900

    feat: android 의존성&빌드 명령어 추가

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ssafy.seoulpop',
+  appName: 'seoul-pop',
+  webDir: 'dist'
+};
+
+export default config;
