82d1d01ea0521cc07b37c544331b9a086fad7466
parkyolo <20181621@kookmin.ac.kr>
2024-04-29 17:53:36 +0900

    feat: 에뮬레이터 서버 url 추가 (#S10P31A409-195)

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ssafy.seoulpop',
   appName: 'seoul-pop',
-  webDir: 'dist'
+  webDir: 'dist',
+  cordova: {},
+  server: {
+    url: 'http://10.0.2.2:5173',
+  },
 };
 
 export default config;
