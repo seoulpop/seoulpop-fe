89f1c7aed00b96da79616a7710b5e3df21d26c6d
dev-hamster <hamsterwave0925@gmail.com>
2024-05-02 00:38:44 +0900

    refactor: 글로벌 스타일 분기 (#S10P31A409-217) (#S10P31A409-217)

---
+++ frontend/src/styles/mainLayout.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+const mainLayout = css`
+  body {
+    position: relative;
+    width: auto;
+    max-width: 480px;
+    min-height: 100svh;
+    margin: 0 auto;
+    padding: 0;
+    background-color: var(--white);
+    font-size: 1.6rem;
+    box-shadow: var(--shadow);
+  }
+`;
+
+export default mainLayout;
