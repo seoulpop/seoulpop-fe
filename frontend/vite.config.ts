c1eedd814b9952ac61bf0e105f37dcd660b0a4b6
dev-hamster <hamsterwave0925@gmail.com>
2024-05-01 22:55:35 +0900

    chore: 로컬 https 환경 (#S10P31A409-217) (#S10P31A409-217)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
@@ -13,5 +15,13 @@ export default defineConfig({
       { find: '@', replacement: '/src' },
       { find: '#', replacement: '/public' },
     ],
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    },
+    // Make sure the server is accessible over the local network
+    host: '0.0.0.0',
   },
 });
