bc3bc3663b51bb3b06f54d8fd47d321b6cc8b299
rglley <foofoo964@naver.com>
2024-05-10 09:25:26 +0900

    chore: 카카오맵(위치) 공통 컴포넌트 추가

---
+++ frontend/src/components/History/KakaoMap.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+interface KakaoMapProps {
+  markerLat: number;
+  markerLng: number;
+  imageSrc: string;
+}
+
+const KakaoMap: React.FC<KakaoMapProps> = ({ markerLat, markerLng, imageSrc }) => {
+  return (
+    <Map
+      center={{ lat: markerLat, lng: markerLng }}
+      style={{ width: '100%', height: '25rem', marginTop: '2rem' }}
+      level={3}
+    >
+      <MapMarker
+        position={{ lat: markerLat, lng: markerLng }}
+        image={{
+          src: imageSrc,
+          size: { width: 70, height: 70 },
+          options: { offset: { x: 27, y: 69 } },
+        }}
+      />
+    </Map>
+  );
+};
+
+export default KakaoMap;
