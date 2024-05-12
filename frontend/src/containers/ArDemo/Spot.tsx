53b0fff63ce028450394deb84ccd3861965b2f1f
dev-hamster <hamsterwave0925@gmail.com>
2024-05-13 01:02:49 +0900

    feat: gps 디버깅 ui (#S10P31A409-336)

--- frontend/src/containers/ArDemo/Spot.tsx
+++ frontend/src/containers/ArDemo/Spot.tsx
@@ -1,8 +1,20 @@
-import { Circle, Entity, Plane, Ring } from '@belivvr/aframe-react';
+import { Circle, Entity, Ring, Text } from '@belivvr/aframe-react';
+import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 
 import { AR_Z_INDEX } from '@/styles/common';
 import { getDistanceFromLatLonInMeters } from '@/utils/distance';
+
+const DebugUI = styled.div`
+  position: fixed;
+  z-index: 1;
+
+  width: 200px;
+  height: 30px;
+  background: #fff;
+
+  font-size: 2rem;
+`;
 
 const formatGpsNewEntityPlace = ({ lat, lng }: { lat: number; lng: number }) =>
   `latitude: ${lat}; longitude: ${lng}`;
@@ -19,13 +31,17 @@ const Spot = ({
   onClickSpot,
   lat = 51.0596, // fake gps
   lng = -0.717,
+  // lat = 127.0877293, // fake gps
+  // lng = 37.4714395,
 }: {
   visible?: boolean;
   lat?: number;
   lng?: number;
   onClickSpot: () => void;
 }) => {
-  const [distance, setDistance] = useState<number>();
+  const [distance, setDistance] = useState('');
+  const [latt, setLat] = useState<number>();
+  const [lngg, setLng] = useState<number>();
 
   useEffect(() => {
     const clickHandler = () => {
@@ -46,7 +62,7 @@ const Spot = ({
 
   useEffect(() => {
     const el = document.getElementById('distance');
-    if (el) el.setAttribute('text', 'value', `${distance}m`); // setAttribute('material', 'color', 'red') https://aframe.io/docs/1.5.0/core/component.html
+    if (el) el.setAttribute('text', 'value', `${distance}km`); // setAttribute('material', 'color', 'red') https://aframe.io/docs/1.5.0/core/component.html
   }, [distance]);
 
   useEffect(() => {
@@ -55,7 +71,11 @@ const Spot = ({
       const data = event as GeolocationCoordinates;
       const { position: curPos } = data.detail;
 
-      console.log('curPos', curPos);
+      console.log(curPos, lat, lng);
+      setLat(curPos.latitude);
+      setLng(curPos.longitude);
+
+      console.log('curPㄴㄴos', curPos, lat, lng);
 
       const dist = getDistanceFromLatLonInMeters({
         lat1: curPos.latitude,
@@ -64,7 +84,7 @@ const Spot = ({
         lon2: lng,
       });
 
-      setDistance(dist);
+      setDistance(dist?.toFixed(2));
       console.log(dist);
     };
 
@@ -76,68 +96,68 @@ const Spot = ({
   }, []);
 
   return (
-    <Entity
-      position={{ x: 0, y: 0, z: AR_Z_INDEX.spot }}
-      visible={visible}
-      gps-new-entity-place={formatGpsNewEntityPlace({ lat, lng })}
-    >
-      <Circle color='#fff' radius={centerRadius} spot-click />
-      <Circle
-        color='#fff'
-        radius={maxRadius}
-        animation__opacity={{
-          property: 'material.opacity',
-          from: '0',
-          to: '0.6',
-          dur: duration,
-          dir: 'alternate',
-          loop: loopInfinity,
-        }}
-        // XXX: scale로 변경
-        animation__radius={{
-          property: 'geometry.radius',
-          from: `${minRadius}`,
-          to: `${maxRadius}`,
-          dur: duration,
-          dir: 'alternate',
-          loop: loopInfinity,
-        }}
-        spot-click
-      />
-      <Ring
-        color='#fff'
-        radiusInner={115}
-        radiusOuter={120}
-        // XXX: scale로 변경
-        animation={{
-          property: 'geometry.radiusInner',
-          from: `${minRadius}`,
-          to: `${maxRadius - ringDelta}`,
-          dur: duration,
-          dir: 'alternate',
-          loop: loopInfinity,
-        }}
-        animation__2={{
-          property: 'geometry.radiusOuter',
-          from: `${minRadius + ringDelta}`,
-          to: `${maxRadius}`,
-          dur: duration,
-          dir: 'alternate',
-          loop: loopInfinity, // XXX: true가 먹지 않음
-        }}
-        spot-click
-      />
-
-      {/* 문화재명, 거리  */}
-      <Entity position={{ x: 0, y: 300, z: 0 }}>
-        <Plane
-          id='distance'
-          height={200}
-          width={1500}
-          text={{ value: '안asdasdj녕?', color: '#000' }}
+    <>
+      <DebugUI>
+        {latt} {lngg}
+      </DebugUI>
+      <Entity
+        position={{ x: 0, y: 0, z: AR_Z_INDEX.spot }}
+        visible={visible}
+        gps-new-entity-place={formatGpsNewEntityPlace({ lat, lng })}
+      >
+        <Circle color='#fff' radius={centerRadius} spot-click />
+        <Circle
+          color='#fff'
+          radius={maxRadius}
+          animation__opacity={{
+            property: 'material.opacity',
+            from: '0',
+            to: '0.6',
+            dur: duration,
+            dir: 'alternate',
+            loop: loopInfinity,
+          }}
+          // XXX: scale로 변경
+          animation__radius={{
+            property: 'geometry.radius',
+            from: `${minRadius}`,
+            to: `${maxRadius}`,
+            dur: duration,
+            dir: 'alternate',
+            loop: loopInfinity,
+          }}
+          spot-click
         />
+        <Ring
+          color='#fff'
+          radiusInner={115}
+          radiusOuter={120}
+          // XXX: scale로 변경
+          animation={{
+            property: 'geometry.radiusInner',
+            from: `${minRadius}`,
+            to: `${maxRadius - ringDelta}`,
+            dur: duration,
+            dir: 'alternate',
+            loop: loopInfinity,
+          }}
+          animation__2={{
+            property: 'geometry.radiusOuter',
+            from: `${minRadius + ringDelta}`,
+            to: `${maxRadius}`,
+            dur: duration,
+            dir: 'alternate',
+            loop: loopInfinity, // XXX: true가 먹지 않음
+          }}
+          spot-click
+        />
+
+        {/* 문화재명, 거리  */}
+        <Entity position={{ x: 0, y: 300, z: 0 }}>
+          <Text id='distance' value='' color='#fff' scale={{ x: 500, y: 500, z: 500 }} />
+        </Entity>
       </Entity>
-    </Entity>
+    </>
   );
 };
 
