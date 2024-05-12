ca409ac1f23b7b5c147d17753442927c1dfa91a1
parkyolo <20181621@kookmin.ac.kr>
2024-05-12 16:50:09 +0900

    feat: background geolocation push nofitication (#S10P31A409-256)

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { useRouteError } from 'react-router-dom';
+
 const NotFoundPage = () => {
+  const error = useRouteError();
+  useEffect(() => {
+    if (
+      import.meta.env.VITE_NODE_ENV === 'development' ||
+      import.meta.env.VITE_NODE_ENV === 'local'
+    ) {
+      console.log('This is error page: ', error);
+    }
+  }, []);
+
   return (
     <div>
       <div>this is not found page</div>
