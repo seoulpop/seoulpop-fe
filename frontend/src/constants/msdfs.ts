13196435092e784a72f206edfc8ddac7608b1a65
hamster-wave <hamsterwave0925@gmail.com>
2024-05-16 12:44:07 +0900

    chore: 숭례문 msdf 추가 (#S10P31A409-365)

--- frontend/src/constants/msdfs.ts
+++ frontend/src/constants/msdfs.ts
@@ -5,9 +5,11 @@ interface HeritageFont {
   [key: string]: string;
 }
 
+// 서울 숭례문
 const HERITAGE_FONT: HeritageFont = {
-  1: `${FILE_PATH}/multicampus.json`,
-  2: `${FILE_PATH}/Gyeongbokgung.json`,
+  '-1': `${FILE_PATH}/sungnyemunGate.json`,
+  '1': `${FILE_PATH}/multicampus.json`,
+  '2': `${FILE_PATH}/gyeongbokgung.json`,
 };
 
 export default HERITAGE_FONT;
