e30d7eaa4443feb9045ce65cee426ad1570e521d
dev-hamster <hamsterwave0925@gmail.com>
2024-05-20 02:56:39 +0900

    feat: pc 레이아웃 (#S10P31A409-313)

--- frontend/src/Layouts/DetailLayout.tsx
+++ frontend/src/Layouts/DetailLayout.tsx
@@ -1,15 +1,21 @@
 import styled from '@emotion/styled';
 
+import PC_LAYOUT, { MOBILE_MIN_WIDTH } from '@/constants/pcLayout';
+
 const DefaultLayout = styled.div`
   position: relative;
   width: auto;
   max-width: 480px;
   min-height: 100svh;
-  margin: 0 auto;
+  margin: 0 0 0 calc(50vw + ${PC_LAYOUT.OFFSET}rem);
   padding: 0;
   background-color: var(--white);
   font-size: 1.6rem;
   box-shadow: var(--shadow);
+
+  @media screen and (min-width: ${MOBILE_MIN_WIDTH}) {
+    margin: 0 auto;
+  }
 `;
 
 export default DefaultLayout;
--- frontend/src/Layouts/MainLayout.tsx
+++ frontend/src/Layouts/MainLayout.tsx
@@ -1,16 +1,22 @@
 import styled from '@emotion/styled';
 
+import PC_LAYOUT, { MOBILE_MIN_WIDTH } from '@/constants/pcLayout';
+
 const MainLayout = styled.div`
   position: relative;
   width: auto;
   max-width: 480px;
   min-height: 100svh;
-  margin: 0 auto;
   padding: 0;
+  margin: 0 auto;
   background-color: var(--white);
   font-size: 1.6rem;
   box-shadow: var(--shadow);
   overflow: hidden;
+
+  @media screen and (min-width: ${MOBILE_MIN_WIDTH}px) {
+    margin: 0 0 0 calc(50vw + ${PC_LAYOUT.OFFSET}rem);
+  }
 `;
 
 export default MainLayout;
